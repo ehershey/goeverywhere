a119b7335e603955caa24aa691c3952f449e72d0
Ernie Hershey <ernie.hershey@10gen.com>
2014-04-18 23:26:19 -0400

    drop duplicates in location unique index

--- createIndexes.js
+++ createIndexes.js
@@ -1,2 +1,2 @@
-db.gps_log.ensureIndex({ entry_date: 1, entry_source: 1, accuracy: 1 }, { unique: true });
+db.gps_log.ensureIndex({ entry_date: 1, entry_source: 1, accuracy: 1 }, { unique: true, dropDups: true });
 db.gps_log.ensureIndex( { loc : "2dsphere" } )
